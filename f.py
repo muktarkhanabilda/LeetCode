ff8fa7cecb1e72c6848d53c13203cbb0383d27f1
Muktarkhan <117581921+muktarkhanabilda@users.noreply.github.com>
2024-11-18 23:17:36 +0600

    Add files via upload

---
+++ f.py
@@ -0,0 +1,11 @@
+n = int(input())
+for i in range(0,n):
+    a = int(input())
+    if a <=10:
+        print("Go to work!")
+    elif 10 < a <= 25:
+        print("You are waek")
+    elif 25 < a <= 45:
+        print("okay , fine")
+    elif a > 45 :
+        print("Burn! Burn! Burn Young!")
